b43ac59b7ec3feaeb864124b6b6787029a0f988a
yash srivastava <yashlala716@gmail.com>
2026-01-16 15:19:36 +0530

    Initial HealAI backend commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import 'dotenv/config'; 
+import "dotenv/config";
 
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.js";
@@ -11,17 +11,24 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:4200", "https://heal-ai-9eab7.web.app"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/chat', chatRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
 
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });   
-}).catch((error) => {
-  console.error('Failed to connect to the database:', error);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+  });
